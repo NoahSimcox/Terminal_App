1dce932da25dfa036b458a41155fcd7fb093ae71
Finnerex <finnerex@gmail.com>
2025-04-12 23:34:40 -0400

    commit

--- obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Terminal_App.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Terminal_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Terminal_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319fa110e7c7085b71846adcfabe17e3785e2f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b7d9494ca0b85d55092451f3eeffdc4aba3c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Terminal_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Terminal_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
